4814e1313052ef9a0478726c19745211e875dbe6
antitomatomato <antitomatomato@gmail.com>
2020-11-23 16:07:16 +0900

    商品出品機能４回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -35,8 +35,6 @@ with_options presence: true do
  validates :shipping_day_id
 end
 
-
-#バリデーション
 with_options numericality: { other_than: 1 } do
   validates :category_id 
   validates :status_id
@@ -48,9 +46,9 @@ end
 validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 
+end
 
 #都道府県(備忘録) 
  #belongs_to :prefecture
  #validates :prefecture_id, presence: true
  #validates :prefecture_id, numericality: { other_than: 1 } 
-end
