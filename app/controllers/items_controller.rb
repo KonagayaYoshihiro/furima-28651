6317229a3b72204b47d5b283820688a90d497e49
antitomatomato <antitomatomato@gmail.com>
2020-11-23 16:38:18 +0900

    商品出品機能５回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
   
   def index
   end
@@ -28,12 +27,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :delivery_price_id, :delivery_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 
 end
