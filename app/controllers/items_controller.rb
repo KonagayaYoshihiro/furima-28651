e035eb57cfbf2880647f9540154c3640d4fd2d15
antitomatomato <antitomatomato@gmail.com>
2020-11-26 21:19:39 +0900

    商品一覧表示機能（ミス）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def destroy
   end
+<<<<<<< Updated upstream
+=======
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :delivery_price_id, :delivery_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def items_params
+    params.require(:item).permit(:image)
+  end
+
+>>>>>>> Stashed changes
 end
