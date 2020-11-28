96099858a9e5d4bc1dc33565288a7eabd4381b83
antitomatomato <antitomatomato@gmail.com>
2020-11-28 21:39:59 +0900

    7回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
   
   def index
-    @items = Item.all
+    @items = Item.all.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items_params = params[:id]
+    #@items_params = params[:id]
   end
 
   def edit
