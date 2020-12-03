552f6f54a695d4719c73902e288060aeeaf0046a
antitomatomato <antitomatomato@gmail.com>
2020-12-03 19:35:11 +0900

    ログイン遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,11 @@
 class OrdersController < ApplicationController
 
   def index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+
+    @item = Item.find(params[:id])
   end
   
 end
