8937eee8a28b0f25b0c62f39b9ce9b2c2606e589
antitomatomato <antitomatomato@gmail.com>
2020-12-05 22:03:04 +0900

    ログインと購入内容確認画面の写真と値段の変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+    # @purchase = フォームオブジェクト.new  @item.find(params[:id])
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to user_session_path
     end
-
-    @item = Item.find(params[:id])
   end
-  
+
 end
